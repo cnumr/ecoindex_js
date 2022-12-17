644de03ac62da3b07a4863b9f7f8fd84048a2695
Thomas Sécher <me@thomas-secher.fr>
2022-12-17 19:03:28 +0100

    Update License and add test (#1)
    * Add Licence
    
    * Update base data getters
    
    * Add test
    
    * Update readme

---
+++ test/ecoIndexGrade.js
@@ -0,0 +1,40 @@
+import {expect} from 'chai';
+import {getEcoIndexGrade} from "../ecoindex.js";
+
+/**
+ * Test getEcoIndexGrade.
+ */
+describe('Compute ecoIndex', () => {
+  // Correct values.
+  it(`Should get ecoIndex grade`, () => {
+    [
+      {value: 81, grade: 'A'},
+      {value: 80, grade: 'B'},
+      {value: 71, grade: 'B'},
+      {value: 70, grade: 'C'},
+      {value: 56, grade: 'C'},
+      {value: 55, grade: 'D'},
+      {value: 41, grade: 'D'},
+      {value: 40, grade: 'E'},
+      {value: 26, grade: 'E'},
+      {value: 25, grade: 'F'},
+      {value: 11, grade: 'F'},
+      {value: 10, grade: 'G'},
+    ].forEach(fixture => {
+      const grade = getEcoIndexGrade(fixture.value);
+      expect(grade).to.be.a('string').to.be.eq(fixture.grade);
+    })
+  });
+
+  // Invalid negative ecoIndex value.
+  it(`Should return false for negative ecoIndex`, () => {
+    const grade = getEcoIndexGrade(-1);
+    expect(grade).to.be.a('boolean').to.be.eq(false);
+  })
+
+  // Invalid more than 100 ecoIndex value.
+  it(`Should return false for ecoIndex greater than 100 `, () => {
+    const grade = getEcoIndexGrade(110);
+    expect(grade).to.be.a('boolean').to.be.eq(false);
+  })
+});
